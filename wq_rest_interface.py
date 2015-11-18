da930cf603a846a2fe30040ad6156def4b2b3dce
DanRamage <dan@inlet.geol.sc.edu>
2015-11-18 12:49:03 -0500

    ADded logging.

--- wq_rest_interface.py
+++ wq_rest_interface.py
@@ -1,7 +1,21 @@
 from flask import Flask
+import logging.config
+import simplejson
 
 app = Flask(__name__)
+app.debug = True
 
+LOGCONFFILE = '/var/www/wq_rest_interface/wq_rest.conf'
+
+FL_SARASOTA_PREDICTIONS_FILE='/mnt/fl_wq/Predictions.json'
+FL_SARASOTA_ADVISORIES_FILE='/mnt/fl_wq/monitorstations/beachAdvisoryResults.json'
+
+SC_MB_PREDICTIONS_FILE=''
+SC_MB_ADVISORIES_FILE=''
+
+logger = None
+if app.debug:
+  logger = logging.config.fileConfig(LOGCONFFILE)
 
 @app.route('/')
 def hello_world():
@@ -11,9 +25,24 @@ def hello_world():
 def get_sarasora_results():
   return 'You asked for Sarasota results.'
 
-@app.route('/myrtlebeach/current_results')
-def get_mb_results():
-  return 'You asked for Myrtle Beach results.'
+
+@app.route('/sarasota/predictions/current_results')
+def get_sarasora_current_results():
+  if logger:
+    logger.debug("get_sarasora_current_results Started.")
+
+  results = {'status': {'http_code': 404}}
+  try:
+    with open(FL_SARASOTA_PREDICTIONS_FILE, 'r') as fl_predictions_file:
+      results = fl_predictions_file.read()
+  except IOError,e:
+    if logger:
+      logger.exception(e)
+
+  if logger:
+    logger.debug("get_sarasora_current_results Finished.")
+
+  return results
 
 if __name__ == '__main__':
   app.run()

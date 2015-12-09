247af2b459cd91372861da9930ca79acb1f15daa
DanRamage <dan@inlet.geol.sc.edu>
2015-12-09 13:58:54 -0500

    Added function to open and process the prediction and sample data files to reduce redundant code.

--- view.py
+++ view.py
@@ -73,6 +73,7 @@ def get_data_file(filename):
   results = {'status': {'http_code': 404},
              'contents': {}}
   ret_code = 404
+
   try:
     with open(filename, 'r') as data_file:
       results['status']['http_code'] = 200

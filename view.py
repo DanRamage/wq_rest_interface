f51a3778b0e6f5ae5fcb00cf258d0abfd9d77011
DanRamage <dan@inlet.geol.sc.edu>
2015-12-09 14:28:16 -0500

    Added function to open and process the prediction and sample data files to reduce redundant code.

--- view.py
+++ view.py
@@ -78,7 +78,7 @@ def get_data_file(filename):
     with open(filename, 'r') as data_file:
       #results['status']['http_code'] = 200
       #results['contents'] = simplejson.load(data_file)
-      results = data_file.read()
+      results = simplejson.load(data_file.read())
       ret_code = 200
 
   except (Exception, IOError) as e:

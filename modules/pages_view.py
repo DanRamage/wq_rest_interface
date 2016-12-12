b8d06fa4cd8a957da43989a00ddbb1c29a894263
DanRamage <ChiefDan@gmail.com>
2016-12-12 11:23:38 -0500

    Updates for blueprint testing.

--- modules/pages_view.py
+++ modules/pages_view.py
@@ -3,7 +3,7 @@
 
 #from main import logger
 import logging.config
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, current_app
 
 pages_view = Blueprint('pages_view', __name__,
                         template_folder='templates')
@@ -11,7 +11,7 @@ pages_view = Blueprint('pages_view', __name__,
 logger = logging.getLogger('wq_rest_logger')
 @pages_view.route('/')
 def root():
-  app = Flask.current_app
+  app = current_app
   #if logger:
   app.logger.debug("root Started.")
   return render_template("intro_page.html")
@@ -19,7 +19,7 @@ def root():
 
 @pages_view.route('/<sitename>')
 def index_page(sitename):
-  app = Flask.current_app
+  app = current_app
   #if logger:
   app.logger.debug("index_page for site: %s" % (sitename))
   if sitename == "myrtlebeach":

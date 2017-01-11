fbc759d3fe576937920f0971194e50235ac1c7b1
DanRamage <ChiefDan@gmail.com>
2017-01-11 14:20:51 -0500

    Restructured

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ SECRET_KEY_FILE = 'secret_key'
 # Create in-memory database
 DATABASE_FILE = 'wq_db.sqlite'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_FILE
-SQLALCHEMY_ECHO = True
+SQLALCHEMY_ECHO = False
 
 #LOGFILE='/Users/danramage/tmp/log/flask_plug_view_site.log'
 LOGFILE='/var/log/wq_rest/flask_plug_view_site.log'

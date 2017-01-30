0dab7f13636770840ac701c258ef75cb276be2e5
DanRamage <ChiefDan@gmail.com>
2017-01-30 11:56:35 -0500

    Added advisory limits model and view.

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ SECRET_KEY_FILE = 'secret_key'
 # Create in-memory database
 DATABASE_FILE = 'wq_db.sqlite'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_FILE
-SQLALCHEMY_ECHO = False
+SQLALCHEMY_ECHO = True
 
 if PYCHARM_DEBUG:
   LOGFILE='/Users/danramage/tmp/log/flask_plug_view_site.log'

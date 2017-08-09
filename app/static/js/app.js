3c28761cc964ae28c56892544311418bfddf77f1
DanRamage <ChiefDan@gmail.com>
2017-08-09 14:55:53 -0400

    Trying to fix message height whitespace at bottom.

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -928,7 +928,7 @@ if(onlineStatus != 'off'){
 
   $('#mapPage').bind('pageshow', function(event) {
 
-    var mapHeight = getScreenSize('height') - (document.getElementById('mainheader').offsetHeight)+2;
+    var mapHeight = getScreenSize('height') - (document.getElementById('mainheader').offsetHeight) - (document.getElementById('mainfooter').offsetHeight) + 2;
 
 
     $('#map_canvas').css( "height", mapHeight );
